683fd38a0d981d9cf55e71f01aa5ce5eefe8f7d7
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-02-21 09:18:07 +0700

    update recruitment schema to validate phone number format; add PendaftaranPage component for registration form

--- backend/src/zod/recruitment.schema.ts
+++ backend/src/zod/recruitment.schema.ts
@@ -4,7 +4,9 @@ export const recruitmentSchema = z.object({
   nama: z.string().nonempty("field nama tidak boleh kosong").min(3).max(255),
   nim: z.string().nonempty().min(10).max(10),
   email: z.string().email("email tidak valid").nonempty(),
-  no_telepon: z.string().nonempty().min(10).max(15),
+  no_telepon: z.string().regex(/^(\+62|62|0)8[1-9][0-9]{6,9}$/, {
+    message: "Nomor telepon tidak valid.",
+  }),
   gender: z.enum(["LAKI_LAKI", "PEREMPUAN"]),
   alamat: z.string().nonempty(),
   motivasi: z.string().nonempty(),
